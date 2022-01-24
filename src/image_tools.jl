ef40e7400682254e33577763e44a3de1dff75c48
James Schloss <jrs.schloss@gmail.com>
2022-01-24 18:57:37 +0100

    adding skeleton for image tools

--- src/image_tools.jl
+++ src/image_tools.jl
@@ -1,28 +1,6 @@
 # TODO: Probably shouldn't print anything anymore...
 using LinearAlgebra
 
-#TODO: figure out how to select the best deviation factor
-function create_gaussian_kernel(kernel_size; deviation_factor = 1)
-    if !isodd(kernel_size)
-        println("Even kernel sizes are not allowed. New kernel size is: ",
-                kernel_size-1)
-        kernel_size -= 1
-    end
-
-    kernel = zeros(kernel_size, kernel_size)
-    center = floor(Int, kernel_size / 2) + 1
-
-    for i = 1:kernel_size
-        for j = 1:kernel_size
-            kernel[i,j] = exp(-((i-center)^2 + (j-center)^2)/
-                              (kernel_size/deviation_factor))
-        end
-    end
-
-    return normalize(kernel)
-    
-end
-
 #TODO: maybe normalize channels altogether, not separate?
 function normalize!(img::Array{C,2}) where {C <: Union{RGB, RGBA}}
 
@@ -54,16 +32,6 @@ function normalize!(img::Array{C,2}) where {C <: Union{RGB, RGBA}}
     
 end
 
-function bounds_check(signal, index, filter_width, dir)
-    if index < 1
-        filter_width - (1-index)
-    elseif index > size(signal)[dir]
-        return filter_width - (index - size(signal)[dir])
-    else
-        return filter_width
-    end
-end
-
 function pixel_sum(signal::Array{Pixel,2})
     rsum = 0
     for i = 1:length(signal)
@@ -73,137 +41,21 @@ function pixel_sum(signal::Array{Pixel,2})
     return rsum
 end
 
-# TODO: the normalization of the colors seems off, creating a spotty output
-function filter_color(filter, signal)
-    if (size(signal) != size(filter))
-        error("filter and image views are not consistent!")
-    end
-
-    rsum = RGB(0)
-    for i = 1:length(signal)
-        rsum += filter[i]*signal[i].c
-    end
-
-    return rsum / sum(filter)
-    
-end
-
-# TODO: this is computationally complex. Kinda sucks for that reason.
-function find_filter_width(signal, threshold, index::CartesianIndex;
-                           max_width = 15)
-
-    num_points = signal[index].val
-
-    filter_width = 1
-
-    while (num_points < threshold) && (filter_width < max_width)
-        filter_width += 2
-
-        num_points = pixel_sum(signal, filter_width, index)
-
-    end
-
-    return filter_width
+function to_rgb(r,g,b)
+    return RGB(r,g,b)
 end
 
 
-# TODO: too many pixel sum functions
-function pixel_sum(signal::Array{Pixel,2}, filter_width::Int,
-                   index::CartesianIndex)
-
-    center = filter_width + 1
-
-    j,i = index[1], index[2]
-
-    # This can create a signal view that is not the size of the filter
-    left_bound = bounds_check(signal, i-filter_width, filter_width, 2)
-    right_bound = bounds_check(signal, i+filter_width, filter_width, 2)
-    top_bound = bounds_check(signal, j+filter_width, filter_width, 1)
-    bottom_bound = bounds_check(signal, j-filter_width, filter_width, 1)
-
-    # TODO: zero-pad view or slize filter
-    #println(filter[1])
-    rsum = pixel_sum(signal[j-bottom_bound:j+top_bound,
-                            i-left_bound:i+right_bound])
-
-    return rsum
-
-end
-
-
-function pixel_sum(signal::Array{Pixel,2}, filter::Array{Float64,2},
-                   index::CartesianIndex)
-
-    filter_width = floor(Int, size(filter)[1]/2)
-    center = filter_width + 1
-
-    j,i = index[1], index[2]
-
-    # This can create a signal view that is not the size of the filter
-    left_bound = bounds_check(signal, i-filter_width, filter_width, 2)
-    right_bound = bounds_check(signal, i+filter_width, filter_width, 2)
-    top_bound = bounds_check(signal, j+filter_width, filter_width, 1)
-    bottom_bound = bounds_check(signal, j-filter_width, filter_width, 1)
-
-    # TODO: zero-pad view or slize filter
-    #println(filter[1])
-    rsum = Pixel(signal[i,j].val,
-                 filter_color(filter[center-bottom_bound:center+top_bound,
-                                     center-left_bound:center+right_bound],
-                              signal[j-bottom_bound:j+top_bound,
-                                     i-left_bound:i+right_bound]))
-
-    return rsum
-
-end
-
-#TODO: figure out what how to make a variable sized filter
-#      We need to read in the point data to figure out filter size
-#TODO: I believe this is actually a correlation, not convolution ^^
-function fractal_conv(signal::Array{Pixel,2}; threshold = 1)
-
-    n = size(signal)
-    out = Array{Pixel,2}(undef,n)
-
-    # time domain
-    for j = 1:n[1]
-        for i = 1:n[2]
-            filter_width = find_filter_width(signal, threshold,
-                                             CartesianIndex(j,i))
-            filter = create_gaussian_kernel(filter_width)
-            rsum = pixel_sum(signal, filter, CartesianIndex(j,i))
-            out[j, i] = rsum
-            rsum = RGB(0)
-        end
-    end
-
-    return out
-end
-
-function mix_color(a::RGB, b::RGB)
-    return RGB(0.5*(a.r+b.r), 0.5*(a.g+b.g), 0.5*(a.b + b.b))
-end
-
-function mix_color!(pix::Pixel, p::Point)
-    pix.c = (pix.c*pix.val + p.c)/(pix.val + 1)
-    pix.val += 1
-end
-
-function to_rgb(pix::Pixel)
-    return pix.c
-end
-
-
-function to_logscale(pix::Pixel, max_val; gamma = 2.2)
+function to_logscale(r,g,b,val, max_val; gamma = 2.2)
     alpha = 1
 
     # TODO: check to see if this is an appropriate log implementation
     # TODO: What's meant by "vibrant colors?"
-    if pix.val != 0
-        alpha = log10((9*pix.val/max_val)+1)
-        #alpha = 1 + log10(pix.val/max_val)
+    if val != 0
+        alpha = log10((9*val/max_val)+1)
+        #alpha = 1 + log10(val/max_val)
     end
-    final_color = RGB(pix.c.r*alpha, pix.c.g*alpha, pix.c.b*alpha)
+    final_color = RGB(r*alpha, g*alpha, b*alpha)
 
     # Applying Gamma
     # TODO: We should be able to broadcast a power operation to clean this up
@@ -211,9 +63,24 @@ function to_logscale(pix::Pixel, max_val; gamma = 2.2)
                       final_color.g^(1/gamma),
                       final_color.b^(1/gamma))
 
-    return Pixel(pix.val, final_color)
+    return final_color
 end
 
+function write_image(pix::Pixels, filename; gamma = 2.2)
+
+    img = Array{RGB,2}(undef, size(pix.values))
+
+    max_val = maximum(pix.values)
+    for i = 1:length(img)
+        img[i] = to_logscale(pix.reds[i], pix.greens[i], pix,blues[i],
+                             pix.values[i], max_val; gamma = gamma)
+    end
+
+    save(filname, img)
+
+end
+
+#=
 function write_image(points::Vector{Point}, ranges, res, filename;
                      point_offset = 10, gamma = 2.2)
 
@@ -260,3 +127,4 @@ function write_image(points::Vector{Point}, ranges, res, filename;
     save(filename, img)
 
 end
+=#
