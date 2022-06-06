4c5b852522e74df8ffc479a4724b9b366353d699
James Schloss <jrs.schloss@gmail.com>
2022-06-06 14:42:23 +0200

    adding new way to create fids

--- src/simple_rng.jl
+++ src/simple_rng.jl
@@ -23,6 +23,88 @@ end
 
 # This is a combination of both
 function simple_rand(x::Union{Int, UInt})
-
     return LCG_step(UInt(x), UInt(2862933555777941757), UInt(1))
+end
+
+@inline function find_fid(prob_set, start, fnum, seed)
+    rnd = seed/typemax(UInt)
+    p = 0.0
+
+    for i = start:start + fnum - 1
+        p += prob_set[i]
+        if rnd <= p
+            return i - start + 1
+        end
+    end
+
+    return 0
+end
+
+# The fid (Function ID) is used to select which operation in a 
+#   function system to select for each iteration.
+# The fid could have multiple function systems in it. Quick example:
+#     2 function systems (fs) with 3 and 5 functions respectively.
+#     The fid might be 011 01, read from right to left
+#     The first 2 digits, 01, represent a selection between 1 and 3 from fs 1
+#     The second 3 digits, 011, represent a selection between 1 and 5 from fs 2
+#     As a UInt, the value is saved as 25
+# encoding an fid involves generating a random UInt and going through the
+#   bitstring to ensure that each set of possible values is a possible option
+function create_fid(rng::UI, fnums) where UI <: Unsigned
+    val = UI(0)
+    offset = UI(0)
+
+    for i = 1:length(fnums)
+        # needed bits to hold the fnum
+        bitsize = ceil(UI, log2(fnums[i]))
+
+        # set of 1's to mask only those bits in the rng string
+        bitmask = UI(2^(bitsize + offset) - 1 - (2^offset - 1))
+
+        # shifting the bits from rng over to read them as an int
+        a = UI(((rng & bitmask) >> offset))
+
+        # checking if that option is actually valid,
+        # if not, using the rng string to create an adhoc new choice
+        if a+1 > fnums[i]
+            a = UI(rng % fnums[i])
+        end
+
+        # Shifting back to appropriate location
+        b = a << offset
+
+        # adding to final bitstring
+        val += b
+
+        # creating offset bit number for next iteration
+        offset += bitsize
+    end
+
+    return val
+end
+
+function create_fid(probs, fnums, seed)
+    fid = UInt(0)
+    bit_offset = 0
+    fx_offset = 1
+    for i = 1:length(fnums)
+        if probs[fx_offset] < 1
+            seed = simple_rand(UInt(seed))
+            choice = find_fid(probs, fx_offset, fnums[i], seed)
+            fid += ((choice-1) << bit_offset)
+            bit_offset += ceil(UInt, log2(fnums[i]))
+        end
+        fx_offset += fnums[i]
+    end
+    return fid
+end
+
+# Decoding takes an offset, which is the number of digits on the fid bitstring
+#   to ignore from the right hand side.
+# Should probably add an error, but was afraid of the conditional
+function decode_fid(fid::UI, offset, fnum) where UI <: Unsigned
+    bitsize = ceil(UI, log2(fnum))
+    bitmask = UI(2^(bitsize + offset) - 1 - (2^offset - 1))
+    value = UI((fid & bitmask) >> offset) + 1
+    return value
 end
--- test/random_tests.jl
+++ test/random_tests.jl
@@ -43,3 +43,42 @@ end
         @test abs(avg-0.5) < threshold
     end
 end
+
+@testset "fid tests" begin
+    fnums = Tuple([UInt16(i) for i = 1:7])
+    rng = typemax(UInt16)
+    fid = Fae.create_fid(rng, fnums)
+
+    @test bitstring(UInt16(fid)) == "0000101100011001"
+
+    probs = (1,
+             1/2, 1/2,
+             1/3, 1/3, 1/3, 
+             1/4, 1/4, 1/4, 1/4,
+             1/5, 1/5, 1/5, 1/5, 1/5, 
+             1/6, 1/6, 1/6, 1/6, 1/6, 1/6,
+             1/7, 1/7, 1/7, 1/7, 1/7, 1/7, 1/7)
+
+    fid_2 = Fae.create_fid(probs, fnums, rng)
+
+    offset = 0
+    for i = 1:length(fnums)
+        choice = Fae.decode_fid(fid, offset, fnums[i])
+        if i in (1,2,4)
+            @test choice == i
+        else
+            @test choice != i
+        end
+
+        choice_2 = Fae.decode_fid(rng, offset, fnums[i])
+
+        @test choice_2 == 2^(ceil(log2(fnums[i])))
+
+        choice_3 = Fae.decode_fid(fid_2, offset, fnums[i])
+        @test choice_3 <= i
+
+        offset += ceil(Int, log2(fnums[i]))
+    end
+
+
+end
