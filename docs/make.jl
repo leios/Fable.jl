3c3fea12b5749bc86968f774db338e953fc07ff6
James Schloss <jrs.schloss@gmail.com>
2022-06-16 10:40:42 +0200

    adding structure to docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,13 @@
 using Documenter, Fae
 
-makedocs(sitename="Fae.jl")
+makedocs(
+    sitename="Fae.jl",
+    authors="James Schloss (Leios) and contributors",
+    pages = [
+        "Home" => "index.md",
+        "General Information" => "general_info.md",
+        "Examples" => Any[
+            "Logo" => "examples/logo.md",
+        ],
+    ],
+)
