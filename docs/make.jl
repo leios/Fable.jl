1dc80dda47fe5215435426d0a6fc86c48bf64fc1
James Schloss <jrs.schloss@gmail.com>
2022-06-17 11:23:26 +0200

    adding research directions and adding docs image to README

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,17 @@
-using Documenter, Fae
+using Documenter
 
 makedocs(
     sitename="Fae.jl",
     authors="James Schloss (Leios) and contributors",
     pages = [
         "Home" => "index.md",
-        "General Information" => "general_info.md",
         "Examples" => Any[
             "Logo" => "examples/logo.md",
         ],
+        "Research Directions" => "research_directions.md",
     ],
 )
 
-deploydocs(;
-    repo="github.com/leios/Fae.jl",
-)
+#deploydocs(;
+#    repo="github.com/leios/Fae.jl",
+#)
