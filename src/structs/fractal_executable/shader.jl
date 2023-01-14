507314ef847271556c7d7cd939544859fd9fa26b
James Schloss <jrs.schloss@gmail.com>
2023-01-14 16:49:30 +0100

    fixing fi issues with hutchinson and shaders

--- src/structs/fractal_executable/shader.jl
+++ src/structs/fractal_executable/shader.jl
@@ -36,13 +36,12 @@ function Shader(a::Shader, b::Shader; mix_function = overlay)
 
     new_fis = vcat(a.fi_set, b.fi_set)
     new_name = a.name*"_"*b.name
-    new_symbols = [a.symbols[i] for i = 1:length(a.symbols)]
-    new_symbols = vcat(symbols, [b.symbols[i] for i = 1:length(b.symbols)])
 
     new_fum = mix_function(a.fum, b.fum)
 
+    symbols = configure_fis!(new_fis)
     return Shader(configure_fum(new_fum; fum_type = :shader, name = new_name),
-                  new_fum, new_fis, new_name, Tuple(new_symbols))
+                  new_fum, new_fis, new_name, symbols)
     
 end
 
@@ -55,20 +54,18 @@ function Shader(shaders::Vector{Shader}; mix_function = mix)
 
     new_fis = shaders[1].fi_set
     new_name = shaders[1].name
-    new_symbols = [shaders[1].symbols[i] for i = 1:length(shaders[1].symbols)]
     all_fums = shaders[1].fum
 
     for i = 2:length(shaders)
         new_fis = vcat(new_fis, shaders[i].fi_set)
         new_name = new_name*"_"*shaders[i].name
-        new_symbols = vcat(new_symbols,
-            [shaders[i].symbols[i] for i = 1:length(shaders[i].symbols)])
         all_fums = vcat(all_fums, shaders[i].fum)
     end
 
+    symbols = configure_fis!(new_fis)
     new_fum = mix_function(all_fums; fum_type = :shader)
     return Shader(configure_fum(new_fum; fum_type = :shader, name = new_name),
-                  new_fum, new_fis, new_name, Tuple(new_symbols))
+                  new_fum, new_fis, new_name, symbols)
 end
 
 function update_fis!(S::Shader, fis::Vector{FractalInput})
