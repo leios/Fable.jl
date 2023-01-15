78ac1fedbb040c2ac2351dcbf6f4c9ed773de7ee
James Schloss <jrs.schloss@gmail.com>
2023-01-15 14:43:20 +0100

    adding new run! function that accepts TimeInterfaces

--- src/run/run.jl
+++ src/run/run.jl
@@ -1,5 +1,14 @@
 export run!
 
+function run!(layer::AbstractLayer, time::TimeInterface; diagnostic = false)
+    run!(layer; diagnostic = diagnostic, frame = frame(time))
+end
+
+function run!(layers::Vector{AbstractLayer}, time::TimeInterface;
+              diagnostic = false)
+    run!(layers; diagnostic = diagnostic, frame = frame(time))
+end
+
 function run!(layers::Vector{AbstractLayer}; diagnostic = false, frame = 0)
     for i = 1:length(layers)
         run!(layers[i]; diagnostic = diagnostic, frame = frame)
