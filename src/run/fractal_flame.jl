leios/Fable.jl

commit 503d4a8c0466982e912b99e2a5f06b44aeabcfca

Shader mixing and fi bug fixes (#50)
* adding structure for mixing color fums

* finishing mixing for hutchinson fums

* adding 'working' shader combinations

* fixing fi issues with hutchinson and shaders

src/structs/fractal_executable/hutchinson.jl
@@ -20,18 +20,15 @@ function update!(final_H::Hutchinson, Hs::HT; diagnostic = false, name = "",
                  final = false) where HT <: Union{Vector{Hutchinson},
                                                   Tuple{Hutchinson}}
 
-    symbols = [Hs[1].symbols[i] for i = 1:length(Hs[1].symbols)]
     prob_set = [Hs[1].prob_set[i] for i = 1:length(Hs[1].prob_set)]
 
     for j = 2:length(Hs)
         temp_prob = [Hs[j].prob_set[i] for i = 1:length(Hs[j].prob_set)]
-        temp_symbols = [Hs[j].symbols[i] for i = 1:length(Hs[j].symbols)]
 
         prob_set = vcat(prob_set, temp_prob)
-        symbols = vcat(symbols, temp_symbols)
     end
 
-    final_H.symbols = Tuple(symbols)
+    update_fis!(final_H)
     final_H.prob_set = Tuple(prob_set)
 
     if diagnostic
@@ -54,7 +51,6 @@ function Hutchinson(Hs::HT; diagnostic = false, name = "",
     fi_set = Hs[1].fi_set
     name_set = Hs[1].name_set
     prob_set = [Hs[1].prob_set[i] for i = 1:length(Hs[1].prob_set)]
-    symbols = [Hs[1].symbols[i] for i = 1:length(Hs[1].symbols)]
     fnums = [Hs[1].fnums[i] for i = 1:length(Hs[1].fnums)]
 
     fsum = length(Hs[1].symbols)
@@ -77,11 +73,9 @@ function Hutchinson(Hs::HT; diagnostic = false, name = "",
         end
 
         temp_prob = [Hs[j].prob_set[i] for i = 1:length(Hs[j].prob_set)]
-        temp_symbols = [Hs[j].symbols[i] for i = 1:length(Hs[j].symbols)]
         temp_fnums = [Hs[j].fnums[i] for i = 1:length(Hs[j].fnums)]
 
         prob_set = vcat(prob_set, temp_prob)
-        symbols = vcat(symbols, temp_symbols)
         fnums = vcat(fnums, temp_fnums)
 
         fsum += length(Hs[j].symbols)
@@ -99,6 +93,7 @@ function Hutchinson(Hs::HT; diagnostic = false, name = "",
             name *= name_set[i] * "_"
         end
     end
+    symbols = configure_fis!(fi_set)
     new_H = configure_hutchinson(fum_set, fi_set, fnums;
                                  name = name, diagnostic = diagnostic,
                                  final = final)
@@ -118,7 +113,7 @@ function Hutchinson(Hs::HT; diagnostic = false, name = "",
 
     return Hutchinson(new_H, new_colors,
                       color_set, fum_set, fi_set, name_set, Tuple(prob_set),
-                      Tuple(symbols), Tuple(fnums))
+                      symbols, Tuple(fnums))
 end
 
 function Hutchinson()
@@ -302,8 +297,8 @@ function Hutchinson(fos::Vector{FractalOperator}; name = "",
                final = final)
 end
 
-function update_fis!(H::Hutchinson, fis::Vector{FractalInput})
-    H.symbols = configure_fis!(fis)
+function update_fis!(H::Hutchinson)
+    H.symbols = configure_fis!(H.fi_set)
 end
 
 function update_colors!(H::Hutchinson, fx_id, h_id,

src/structs/fractal_executable/shader.jl
@@ -18,13 +18,54 @@ end
 
 function Shader(fum::FractalUserMethod, fis::Vector{FractalInput};
                 name = "shader")
-    return Shader(configure_fum(fum; fum_type = :shader, name = name), fum,
+    return Shader(configure_fum(fum, fis; fum_type = :shader, name = name), fum,
                   fis, name, configure_fis!(fis))
 end
 
 function Shader(fum::FractalUserMethod; name = "shader")
     return Shader(configure_fum(fum; fum_type = :shader, name = name), fum,
                   Vector{FractalInput}(), name, Tuple(1))
+end
+
+function Shader(a::Shader, b::Shader; mix_function = overlay)
+    if mix_function != overlay && mix_function != mix
+        @warn("Shader mix type "*string(mix_function)*" not found!\n"*
+              "Defaulting to standard mixing...")
+        mix_function = mix
+    end
+
+    new_fis = vcat(a.fi_set, b.fi_set)
+    new_name = a.name*"_"*b.name
+
+    new_fum = mix_function(a.fum, b.fum)
+
+    symbols = configure_fis!(new_fis)
+    return Shader(configure_fum(new_fum; fum_type = :shader, name = new_name),
+                  new_fum, new_fis, new_name, symbols)
+    
+end
+
+function Shader(shaders::Vector{Shader}; mix_function = mix)
+    if mix_function != overlay && mix_function != mix
+        @warn("Shader mix type "*string(mix_function)*" not found!\n"*
+              "Defaulting to standard mixing...")
+        mix_function = mix
+    end
+
+    new_fis = shaders[1].fi_set
+    new_name = shaders[1].name
+    all_fums = shaders[1].fum
+
+    for i = 2:length(shaders)
+        new_fis = vcat(new_fis, shaders[i].fi_set)
+        new_name = new_name*"_"*shaders[i].name
+        all_fums = vcat(all_fums, shaders[i].fum)
+    end
+
+    symbols = configure_fis!(new_fis)
+    new_fum = mix_function(all_fums; fum_type = :shader)
+    return Shader(configure_fum(new_fum; fum_type = :shader, name = new_name),
+                  new_fum, new_fis, new_name, symbols)
 end
 
 function update_fis!(S::Shader, fis::Vector{FractalInput})

src/structs/fractal_user_methods.jl
@@ -1,4 +1,4 @@
-export FractalUserMethod, @fum, configure_fum
+export FractalUserMethod, @fum, configure_fum, overlay, mix
 
 struct FractalUserMethod
     name::Symbol
@@ -117,10 +117,12 @@ function configure_fum(fum::FractalUserMethod, fis::Vector{FractalInput};
     else
         error("FUM type "*string(fum_type)*" not available!")
     end
-    if fum_type == :color || fum_type == :hutchinson
+
+    if fum_type == :hutchinson
         fx_string *= "x = _p[tid, 2] \n"
         fx_string *= "y = _p[tid, 1] \n"
     end
+
     if fum_type == :color || fum_type == :shader
         fx_string *= "red = _clr[tid, 1] \n"
         fx_string *= "green = _clr[tid, 2] \n"
@@ -185,3 +187,106 @@ function (a::Fae.FractalUserMethod)(args...; kwargs...)
     return FractalUserMethod(Symbol(new_name), a.args, new_kwargs, a.body)
 end
 
+function overlay(a::FractalUserMethod, b::FractalUserMethod; fum_type = :color)
+    new_args = vcat(a.args, b.args)
+    new_kwargs = vcat(a.kwargs, b.kwargs)
+    new_name = Meta.parse(string(a.name)*"_"*string(b.name))
+    new_body = string(b.body)[1:end-3]
+    if fum_type == :color || fum_type == :shader
+        new_body *= "
+            _clr[tid, 1] = red*alpha + (1-alpha)*_clr[tid, 1]\n
+            _clr[tid, 2] = green*alpha + (1-alpha)*_clr[tid, 2]\n
+            _clr[tid, 3] = blue*alpha + (1-alpha)*_clr[tid, 3]\n
+            _clr[tid, 4] = max(alpha, _clr[tid, 4])\n"*
+            string(a.body)[1+5:end]
+    elseif fum_type == :hutchinson
+        new_body *= string(a.body)[1+5:end]
+    end
+
+    return FractalUserMethod(new_name, new_args, new_kwargs,
+                             Meta.parse(new_body))
+end
+
+function mix(a::FractalUserMethod, b::FractalUserMethod; fum_type = :color)
+    new_args = vcat(a.args, b.args)
+    new_kwargs = vcat(a.kwargs, b.kwargs)
+    new_name = Meta.parse(string(a.name)*"_"*string(b.name))
+    new_body = string(b.body)[1:end-3]
+    if fum_type == :color || fum_type == :shader
+        new_body *= "
+            _clr[tid, 1] = red*0.5 + _clr[tid, 1]*0.5\n
+            _clr[tid, 2] = green*0.5 + _clr[tid, 2]*0.5\n
+            _clr[tid, 3] = blue*0.5 + _clr[tid, 3]*0.5\n
+            _clr[tid, 4] = max(alpha, _clr[tid, 4])\n"*
+            string(a.body)[1+5:end]
+    elseif fum_type == :hutchinson
+        new_body *= string(a.body)[1+5:end]
+    end
+
+    return FractalUserMethod(new_name, new_args, new_kwargs,
+                             Meta.parse(new_body))
+
+end
+
+function mix(fums::Vector{FractalUserMethod}; fum_type = :color,
+             alphas::Union{Nothing, Vector{N}} = nothing) where N <: Number
+
+    if fum_type == :color || fum_type == :shader
+        if length(fums) != length(alphas)
+            @warn(string(length(fums))*" FUMs provided with only "*
+                  string(length(alphas))*" alpha values!\n" *
+                  "Mixing all colors evenly...")
+            alphas = [1/length(fums) for i = 1:length(fums)]
+        end
+
+        if !isapprox(sum(alphas),1)
+            @warn("Mixed FUM alpha values != 1!\n"*
+                  "Mixing all colors evenly...")
+            alphas = [1/length(fums) for i = 1:length(fums)]
+        end
+    end
+
+    if length(fums) == 1
+        return fums[1]
+    end
+
+    new_args = fums[1].args
+    new_kwargs = fums[1].kwargs
+    new_name = fums[1].name
+    new_body = string(fums[1].body)[1:end-3]
+    if fum_type == :color || fum_type == :shader
+        new_body *="
+            _clr[tid, 1] = red*"*string(alphas[1])*"\n
+            _clr[tid, 2] = green*"*string(alphas[1])*"\n
+            _clr[tid, 3] = blue*"*string(alphas[1])*"\n
+            _clr[tid, 4] = "*string(alphas[1])*"\n"
+    end
+
+    for i = 2:length(fums)
+        new_args = vcat(new_args, fums[i].args)
+        new_kwargs = vcat(new_kwargs, fums[i].kwargs)
+        new_name = Meta.parse(string(new_name)*"_"*string(fums[i].name))
+        if fum_type == :color || fum_type == :shader
+            new_body *= string(fums[2].body)[1+5:end-3]*"
+                _clr[tid, 1] += red*"*string(alphas[i])*"\n
+                _clr[tid, 2] += green*"*string(alphas[i])*"\n
+                _clr[tid, 3] += blue*"*string(alphas[i])*"\n
+                _clr[tid, 4] += "*string(alphas[i])*"\n"
+        elseif fum_type == :hutchinson
+            new_body *= string(fums[2].body)[1+5:end]
+        end
+    end
+
+    if fum_type == :color || fum_type == :shader
+        new_body *= "
+            red = _clr[tid, 1]
+            green = _clr[tid, 2]
+            blue = _clr[tid, 3]
+            alpha = _clr[tid, 4]
+            end\n"
+    end
+
+    return FractalUserMethod(new_name, new_args, new_kwargs,
+                             Meta.parse(new_body))
+
+end

commit aac583e12fa5c25e81b1094d9a5cc96f6616e3c4

fixing a few small bugs introduced in #50

src/run/fractal_flame.jl
@@ -31,7 +31,9 @@ end
 function iterate!(ps::Points, layer::FractalLayer, H::Hutchinson, n,
                   bounds, bin_widths, H2::Union{Nothing, Hutchinson};
                   frame = 0, diagnostic = false)
-    if !isnothing(H2) && layer.params.solver_type == :semi_random
+    if isnothing(H2) 
+        fx = naive_chaos_kernel!
+    elseif layer.params.solver_type == :semi_random
         fx = semi_random_chaos_kernel!
     elseif layer.params.solver_type == :random
         fx = naive_chaos_kernel!

src/structs/fractal_executable/hutchinson.jl
@@ -302,6 +302,10 @@ function update_fis!(H::Hutchinson)
     H.symbols = configure_fis!(H.fi_set)
 end
 
+function update_fis!(H::Hutchinson, fis::Vector{FractalInput})
+    H.symbols = configure_fis!(fis)
+end
+
 function update_colors!(H::Hutchinson, fx_id, h_id,
                         new_color::Union{RGB, RGBA, Vector{N}}) where N<:Number
 

test/layering_tests.jl
@@ -252,6 +252,8 @@ function layering_testsuite(ArrayType::Type{AT}) where AT <: AbstractArray
 
     @testset "Layering tests for $(string(ArrayType))s" begin
         layering_tests(ArrayType)
+    end
+    @testset "Rescaling tests for $(string(ArrayType))s" begin
         rescaling_tests(ArrayType)
     end
 