43738c89ac1a1573fcb8147f6181105e92b7157b
James Schloss <jrs.schloss@gmail.com>
2023-06-27 14:54:23 +0200

    updating fo interface

--- src/structs/fractal_operators.jl
+++ src/structs/fractal_operators.jl
@@ -1,19 +1,52 @@
 export FractalOperator, fo
 
 struct FractalOperator
-    op::FractalUserMethod
-    color::Union{FractalUserMethod, Tuple}
-    prob::Number
+    ops::Tuple
+    colors::Tuple
+    probs::Tuple
 end
 
-Base.length(fo::FractalOperator) = 1
 fo(args...; kwargs...) = FractalOperator(args...; kwargs...)
 
 function FractalOperator(f::FractalUserMethod, c::FractalUserMethod)
-    return FractalOperator(f, c, 1.0)
+    return FractalOperator((f,), (c,), (1.0,))
 end
 
-FractalOperator(f::FractalUserMethod) = FractalOperator(f,
-                                                        Shaders.previous,
-                                                        1.0)
-FractalUserMethod(f::FractalOperator) = f.op
+FractalOperator(f::FractalUserMethod) = FractalOperator((f,),
+                                                        (Shaders.previous,),
+                                                        (1.0,))
+function FractalOperator(fums, colors, probs)
+    if !isapprox(sum(probs), 1)
+        error("Fractal Operator probability != 1")
+    end
+
+    if length(fums) != length(colors) || length(fums) != length(probs)
+        error("Fractal Operators must have a color and probability"*
+              " for each function!")
+    end
+
+    return FractalOperator(Tuple(fums), Tuple(colors), Tuple(probs))
+end
+
+function FractalOperator(fos::T) where T <: Union{Vector{FractalOperator},
+                                                  Tuple}
+    fxs = fos[1].ops
+    clrs = fos[1].colors
+
+    if !isapprox(sum(fos[1].probs), 1)
+        error("Fractal Operator probability != 1")
+    end
+    probs = fos[1].probs
+
+    for i = 2:length(fos)
+        fxs = (fxs, fos[i].ops)
+        clrs = (clrs, fos[i].colors)
+
+        if !isapprox(sum(fos[i].probs), 1)
+            error("Fractal Operator probability != 1")
+        end
+        probs = (probs, fos[i].probs)
+    end
+
+    return FractalOperator(fxs, clrs, probs)
+end
