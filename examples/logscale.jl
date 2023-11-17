387161271134355135cbf24ea839bae1ddfbb4a5
James Schloss <jrs.schloss@gmail.com>
2023-11-17 20:34:40 +0100

    calc_max_value defaults to logscale

---
+++ examples/logscale.jl
@@ -0,0 +1,24 @@
+using Fable
+
+function logscale_example(num_particles, num_iterations; ArrayType = Array)
+
+    world_size = (9*0.15, 16*0.15)
+    ppu = 1920/world_size[2]
+
+    circle = define_circle(chosen_fx = :naive_disk, color = Shaders.white)
+
+    flayer = FractalLayer(; ArrayType = ArrayType, H = circle,
+                          world_size = world_size, ppu = ppu,
+                          num_particles = num_particles,
+                          num_iterations = num_iterations,
+                          logscale = true, overlay = false)
+
+    run!(flayer)
+
+    #write_image(flayer; filename = "out.png")
+    return write_image(flayer)
+end
+
+@info("Created Function: logscale_example(num_particles, num_iterations;
+                                         ArrayType = Array)")
+
